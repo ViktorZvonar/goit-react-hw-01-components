c5a2829dadb54b9b9eecd82155fdb26b888af4a1
ViktorZvonar <viktorzvonar@yahoo.com>
2023-01-15 00:41:07 +0200

    table-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from 'user.json';
 import data from 'components/Statistics/data.json';
 import friends from 'components/Friends/friends.json';
+import transactions from 'components/TransactionHistory/transactions.json';
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/Friends/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   const { username, tag, location, avatar, stats } = user;
@@ -29,7 +31,8 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
